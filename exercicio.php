facbf5f2713be2264e4c28590b41dbfc5c3e7071
MatheusSouzaCS <mathsowza@outlook.com>
2022-10-18 13:31:42 -0300

    adicionando exercicios

---
+++ exercicio.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="exercicios.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Silkscreen:wght@400;700&display=swap" rel="stylesheet">
+    <title>Exercícios</title>
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+
+        </div>
+    </main>
+    <footer class="rodape">
+        Desenvolvido por Matheus © <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
